ca97c9333d22bd1939c8b7a0fbd5fbb8038bc4eb
richmtx <martinezhernandezricardo06@gmail.com>
2026-02-09 12:45:54 -0600

    Commit

--- src/app/pages/contacto/contacto.component.ts
+++ src/app/pages/contacto/contacto.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from "../../components/footer/footer.component";
+import { MapaComponent } from "../../components/mapa/mapa.component";
 
 @Component({
   selector: 'app-contacto',
-  imports: [FooterComponent],
+  imports: [FooterComponent, MapaComponent],
   templateUrl: './contacto.component.html',
   styleUrl: './contacto.component.css'
 })
--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from "../../components/footer/footer.component";
+import { MapaComponent } from "../../components/mapa/mapa.component";
 
 @Component({
   selector: 'app-index',
-  imports: [FooterComponent],
+  imports: [FooterComponent, MapaComponent],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
