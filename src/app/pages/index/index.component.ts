4a07a6602d1f6431953acd337e71ac55f68c6b85
richmtx <martinezhernandezricardo06@gmail.com>
2026-02-10 09:50:07 -0600

    Commit

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, QueryList, ViewChildren } from '@angular/core';
 import { FooterComponent } from "../../components/footer/footer.component";
 import { MapaComponent } from "../../components/mapa/mapa.component";
 
@@ -8,6 +8,31 @@ import { MapaComponent } from "../../components/mapa/mapa.component";
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
-export class IndexComponent {
+export class IndexComponent implements AfterViewInit {
 
+  // Selecciona todos los elementos con la clase .reveal-event
+  @ViewChildren('revealEvent') revealEvents!: QueryList<ElementRef<HTMLElement>>;
+
+  private observer!: IntersectionObserver;
+
+  ngAfterViewInit(): void {
+    this.observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('active');
+            this.observer.unobserve(entry.target);
+          }
+        });
+      },
+      {
+        threshold: 0.2
+      }
+    );
+
+    // Observamos cada elemento
+    this.revealEvents.forEach(event => {
+      this.observer.observe(event.nativeElement);
+    });
+  }
 }
