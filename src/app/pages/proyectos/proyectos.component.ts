70d23992cc8d296dfe479d8288e2def94e6ebcef
richmtx <martinezhernandezricardo06@gmail.com>
2026-02-10 14:47:18 -0600

    Commit

--- src/app/pages/proyectos/proyectos.component.ts
+++ src/app/pages/proyectos/proyectos.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
@@ -7,6 +8,29 @@ import { FooterComponent } from "../../components/footer/footer.component";
   templateUrl: './proyectos.component.html',
   styleUrl: './proyectos.component.css'
 })
-export class ProyectosComponent {
+export class ProyectosComponent implements AfterViewInit {
 
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngAfterViewInit(): void {
+    if (!isPlatformBrowser(this.platformId)) return;
+
+    document.body.classList.add('js-enabled');
+
+    const elements: NodeListOf<HTMLElement> =
+      document.querySelectorAll('.fade-up, .fade-left');
+
+    const observer = new IntersectionObserver(
+      entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            (entry.target as HTMLElement).classList.add('show');
+          }
+        });
+      },
+      { threshold: 0.2 }
+    );
+
+    elements.forEach(el => observer.observe(el));
+  }
 }
